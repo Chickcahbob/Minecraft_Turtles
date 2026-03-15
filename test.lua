1b57e6c1dd283ca6d27324174e573279f10e0738
Andrew <>
2026-03-14 19:52:21 -0500

    Initial loop for item selection

--- test.lua
+++ test.lua
@@ -1,2 +1,18 @@
+
 turtle.refuel()
 turtle.forward()
+
+local data
+
+function fuel
+    for i=0,15 do
+        turtle.select(i)
+        turtle.getSelectedSlot()
+        data = turtle.getItemDetail()
+        if data then
+            print( "Item name: ", data.name)
+        end
+        
+    end
+    turtle.select(0)
+end
