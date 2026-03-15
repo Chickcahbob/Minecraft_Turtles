836f6b386738aeb015f8c5952988986502c3607d
Andrew <>
2026-03-14 20:07:12 -0500

    Checking if data.name is stored as a string

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ function fuel()
         turtle.select(i)
         turtle.getSelectedSlot()
         data = turtle.getItemDetail()
-        if data then
+        if data.name == "minecraft:coal" || data.name == "minecraft:charcoal" then
             print( "Item name: ", data.name)
         end
         
