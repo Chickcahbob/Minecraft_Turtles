606ea83d7853adc1ba3da563a04fe6736d54464d
Andrew <>
2026-03-14 20:49:28 -0500

    Testing placing a sapling

--- test.lua
+++ test.lua
@@ -4,7 +4,6 @@ function getItem( index )
     turtle.select(index)
     turtle.getSelectedSlot()
     data = turtle.getItemDetail()
-
 end
 
 function placeSapling()
@@ -12,17 +11,18 @@ function placeSapling()
         getItem(i)
         if data then
             if string.find(data.name, "sapling") then
-                print( "Item Name: ", data.name)
-                
+                turtle.up()
+                turtle.placeDown()
+                break
             end
         end
     end
+    turtle.select(1)
 end
 
 function refuel()
     for i=1,16 do
-        turtle.select(i)
-        data = turtle.getItemDetail()
+        getItem(i)
         if data then
             if data.name == "minecraft:coal" or data.name == "minecraft:charcoal" then
                 turtle.refuel()
